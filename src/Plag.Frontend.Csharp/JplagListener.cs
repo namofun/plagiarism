f93729b08c1563c43573ece9f4c3d4f7fc68d17c
小羊 <tlylz99@live.cn>
2020-05-13 14:21:33 +0800

    支持了 C#6 语法。(close #4)

---
+++ src/Plag.Frontend.Csharp/JplagListener.cs
@@ -0,0 +1,9 @@
+﻿using Antlr4.Grammar.Csharp;
+
+namespace Plag.Frontend.Csharp
+{
+    public class JplagListener : CSharpParserBaseListener
+    {
+        public JplagListener(Structure s) { }
+    }
+}
---
+++ src/Plag.Frontend.Csharp/Language.cs
@@ -0,0 +1,63 @@
+﻿using Antlr4.Grammar.Csharp;
+using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Plag.Frontend.Csharp
+{
+    public class Language : ILanguage
+    {
+        public IReadOnlyCollection<string> Suffixes { get; } = new[] { ".CS" };
+
+        public string Name => "C#8";
+
+        public string ShortName => "csharp8";
+
+        public int MinimalTokenMatch => 12;
+
+        public Func<Structure, ICSharpParserListener> ListenerFactory { get; }
+
+        public bool SupportsColumns => true;
+
+        public bool IsPreformated => true;
+
+        public bool UsesIndex => true;
+
+        public int CountOfTokens => (int)TokenConstants.NUM_DIFF_TOKENS;
+
+        public Language() : this(s => new JplagListener(s))
+        {
+        }
+
+        public Language(Func<Structure, ICSharpParserListener> listenerImpl)
+        {
+            ListenerFactory = listenerImpl;
+        }
+
+        public Structure Parse(ISubmissionFile files)
+        {
+            var structure = new Structure();
+            var outputWriter = new StringWriter(structure.OtherInfo);
+            var errorWriter = new StringWriter(structure.ErrorInfo);
+            var listener = ListenerFactory(structure);
+
+            foreach (var item in SubmissionComposite.ExtendToLeaf(files))
+            {
+                var lexer = new CSharpLexer(item.Open(), outputWriter, errorWriter);
+                var parser = new CSharpParser(new CommonTokenStream(lexer), outputWriter, errorWriter);
+            
+                parser.AddErrorListener(structure);
+                parser.AddParseListener(listener);
+
+                var root = parser.CompilationUnit();
+                parser.ErrorListeners.Clear();
+                parser.ParseListeners.Clear();
+            }
+
+            return structure;
+        }
+
+        public string TypeName(int type) => Token.TypeToString((TokenConstants)type);
+    }
+}
