76507a1bc66ddf200b3d6f9218a3e8dad704698b
小羊 <tlylz99@live.cn>
2020-06-03 22:58:32 +0800

    查重报告的生成。

--- src/Data.Abstraction/Match/MatchPair.cs
+++ src/Data.Abstraction/Match/MatchPair.cs
@@ -1,28 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SatelliteSite.Data.Match
+﻿namespace SatelliteSite.Data.Match
 {
+    /// <summary>
+    /// 代码段匹配记录
+    /// </summary>
     public class MatchPair
     {
-        public int StartA { set; get; }
-        public int StartB { set; get; }
-        public int Length { set; get; }
-        public int ContentStartA { set; get; }
-        public int ContentEndA { set; get; }
-        public int ContentStartB { set; get; }
-        public int ContentEndB { set; get; }
+        /// <summary>
+        /// 文件A编号
+        /// </summary>
+        public int FileA { get; set; }
 
-        public MatchPair(int startA, int startB, int length,int contentStartA,int contentEndA,int contentStartB,int contentEndB)
-        {
-            StartA = startA;
-            StartB = startB;
-            Length = length;
-            ContentStartA = contentStartA;
-            ContentStartB = contentStartB;
-            ContentEndA = contentEndA;
-            ContentEndB = contentEndB;
-        }
+        /// <summary>
+        /// 文件B编号
+        /// </summary>
+        public int FileB { get; set; }
+
+        /// <summary>
+        /// 文件A的字符起始地址
+        /// </summary>
+        public int ContentStartA { set; get; }
+
+        /// <summary>
+        /// 文件A的字符中止地址
+        /// </summary>
+        public int ContentEndA { set; get; }
+
+        /// <summary>
+        /// 文件B的字符起始地址
+        /// </summary>
+        public int ContentStartB { set; get; }
+
+        /// <summary>
+        /// 文件B的字符终止地址
+        /// </summary>
+        public int ContentEndB { set; get; }
     }
 }
