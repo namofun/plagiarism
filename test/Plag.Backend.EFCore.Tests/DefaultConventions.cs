097a649c26d537917b55f44df25e25ee3b48f85c
XiaoYang <670171961@qq.com>
2021-02-24 16:26:02 +0800

    Add tests for default conventions.

---
+++ test/Plag.Backend.EFCore.Tests/DefaultConventions.cs
@@ -0,0 +1,82 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Plag.Backend.Entities;
+using Plag.Backend.Services;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Plag.Backend.Tests
+{
+    public class Database : DbContext
+    {
+        public Database(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new PlagEntityConfiguration<Database>().Configure(modelBuilder);
+        }
+    }
+
+    public class ServiceProviderFixture : IServiceProvider, IDisposable
+    {
+        public ServiceProvider ServiceProvider { get; }
+
+        public void Dispose() => ServiceProvider?.Dispose();
+
+        public object GetService(Type serviceType)
+        {
+            return ((IServiceProvider)ServiceProvider).GetService(serviceType);
+        }
+
+        public ServiceProviderFixture()
+        {
+            var services = new ServiceCollection();
+            services.AddDbContext<Database>(b => b.UseInMemoryDatabase("PlagDevTests", b => b.UseBulk()));
+            services.AddSingleton<ICompileService, NullCompileService>();
+            services.AddSingleton(typeof(IResettableSignal<>), typeof(NullResettableSignal<>));
+            services.AddScoped<IPlagiarismDetectService, EntityFrameworkCoreStoreService<Database>>();
+            ServiceProvider = services.BuildServiceProvider(true);
+        }
+    }
+
+    public class DefaultConventions : IClassFixture<ServiceProviderFixture>
+    {
+        private IServiceProvider ServiceProvider { get; }
+
+        public DefaultConventions(ServiceProviderFixture serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        [Fact]
+        public async Task NotExists_ReturnsNull()
+        {
+            using var scope = ServiceProvider.CreateScope();
+            var pds = scope.ServiceProvider.GetRequiredService<IPlagiarismDetectService>();
+            var nullGuid = Guid.Empty.ToString();
+
+            Assert.Null(await pds.FindSubmissionAsync(nullGuid, 0, true));
+            Assert.Null(await pds.FindSetAsync(nullGuid));
+            Assert.Null(await pds.FindReportAsync(nullGuid));
+            Assert.Null(await pds.FindLanguageAsync("cpp"));
+            Assert.Null(await pds.GetComparisonsBySubmissionAsync(nullGuid, 0));
+            Assert.Null(await pds.GetCompilationAsync(nullGuid, 0));
+            Assert.Null(await pds.GetFilesAsync(nullGuid, 0));
+        }
+
+        [Fact]
+        public async Task NotExists_ThrowsException()
+        {
+            using var scope = ServiceProvider.CreateScope();
+            var pds = scope.ServiceProvider.GetRequiredService<IPlagiarismDetectService>();
+            var nullGuid = Guid.Empty.ToString();
+
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => pds.SubmitAsync(new Models.SubmissionCreation { SetId = nullGuid }));
+        }
+    }
+}
---
+++ test/Plag.Backend.EFCore.Tests/Nulls/NullResettableSignal.cs
@@ -0,0 +1,23 @@
+﻿using Plag.Backend.Services;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Plag.Backend.Tests
+{
+    public class NullResettableSignal<T> : IResettableSignal<T>
+        where T : IResettableService
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Notify()
+        {
+        }
+
+        public Task WaitAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
