7cd155c4f4b44814e2517cf85fb23d706b8ef51b
XiaoYang <tlylz99@live.cn>
2021-08-22 12:03:20 +0800

    Add react project to see it in devenv.

--- src/React.UI/src/App.tsx
+++ src/React.UI/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router';
-import { NavLink, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { RouteComponentProps, NavLink, Route } from 'react-router-dom';
 import { Action, Location } from 'history';
 
 import logo from './logo.svg';
