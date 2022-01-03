2e7ba9e70635ce2ba52ad2615cdb06206b97bda4
XiaoYang <tlylz99@live.cn>
2022-01-03 17:04:35 +0800

    Move entities to relational project since they are not useful for documental project

--- src/Plag.Backend.Abstraction/Services/JobContext.cs
+++ src/Plag.Backend.Abstraction/Services/JobContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Plag.Backend.Jobs
+namespace Plag.Backend.Services
 {
     /// <summary>
     /// The contract for finishing jobs.
--- src/Plag.Backend.Abstraction/Services/LruStore.cs
+++ src/Plag.Backend.Abstraction/Services/LruStore.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Plag.Backend.Jobs
+namespace Plag.Backend.Services
 {
     public class LruStore<TKey, TValue>
     {
