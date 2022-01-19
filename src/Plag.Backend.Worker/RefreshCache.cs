d15993bfc9cffec095ce5f847cec5ac1b8acd3e4
XiaoYang <tlylz99@live.cn>
2022-01-19 23:15:01 +0800

    Add refresh-cache util.

---
+++ src/Plag.Backend.Worker/RefreshCache.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Plag.Backend.Services;
+using System.Threading.Tasks;
+
+namespace Plag.Backend.Worker
+{
+    public class RefreshCache
+    {
+        private readonly IJobContext _jobContext;
+
+        public RefreshCache(IJobContext jobContext)
+        {
+            _jobContext = jobContext;
+        }
+
+        [FunctionName("RefreshCache")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger("post", Route = "refresh-cache")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Refreshing cache for the system.");
+            await _jobContext.RefreshCacheAsync();
+            return new OkObjectResult(new { result = "ok" });
+        }
+    }
+}
